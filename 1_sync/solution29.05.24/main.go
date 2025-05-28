d5b0121552ce14768b77e327621d108fa6d24a84
piftai <piftaiwork@gmail.com>
2025-05-29 01:47:24 +0300

    first task done

---
+++ 1_sync/solution29.05.24/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// print square of range 0...20 in random order
+func main() {
+	counter := 20
+	wg := sync.WaitGroup{}
+	for i := 0; i < counter; i++ {
+		wg.Add(1)
+		go func(in int) {
+			defer wg.Done()
+			fmt.Println(in * in)
+		}(i)
+	}
+
+	wg.Wait()
+}
